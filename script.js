b949eb8e6a6fc1abebc7d8ee7cf3ae1a1dc023bd
Lisa Berg <lisa.bergmohn@gmail.com>
2023-12-28 14:50:09 +0100

    Second commit

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function submitForm(event) {
     event.preventDefault();
 
-    let firstname = document.getElementById('firstname').value;
-    let lastname = document.getElementById('lastname').value;
+    let fname = document.getElementById('fname').value;
+    let lname = document.getElementById('lname').value;
     let email = document.getElementById('email').value;
-    let area = document.getElementById('area').value;
+    let subject = document.getElementById('subject').value;
 
-    if(firstname !== '' && lastname !== '' && email !== '' && area !== '') { 
-        document.getElementById('firstname').value = '';
-        document.getElementById('lastname').value = '';
+    if(fname !== '' && lname !== '' && email !== '' && subject !== '') { 
+        document.getElementById('fname').value = '';
+        document.getElementById('lname').value = '';
         document.getElementById('email').value = '';
-        document.getElementById('area').value = '';
+        document.getElementById('subject').value = '';
     
         alert('Your message is submitted'); 
 
